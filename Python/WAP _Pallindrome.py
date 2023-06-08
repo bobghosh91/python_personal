45473ed8c3c0e980c7fd541b3d36ac477a71c16f
Bob Ghosh <bob.ghosh@sapiens.com>
2023-06-08 11:19:14 +0530

    commitLenovo

--- Python/WAP _Pallindrome.py
+++ Python/WAP _Pallindrome.py
@@ -1,7 +1,12 @@
-str = 'madam'
-new_str = str[::-1]
 
-if (str == new_str):
-    print('is pallindrome')
-else:
-    print('ain\'t no b')
+def checkStringPallindrome(str1='madam'):
+
+    new_str = str1[::-1]
+
+    if (str1 == new_str):
+        print('string is pallindrome')
+    else:
+        print('ain\'t no b')
+
+
+checkStringPallindrome('arjun')
